59aa51eb2498c883c047fb91102c902a89cc7716
AlexandreDresch <z3r0pc@protonmail.com>
2022-05-12 23:23:27 -0300

    Header update

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,35 @@
 import { View, Text } from 'react-native';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import { styles } from './styles';
 
 export function Home(){
   return (
     <View style={styles.container}>
-        <Text style={styles.title}>
-            Home
-        </Text>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+
+      <View style={styles.header}>
+        <View style={styles.headerContent}>
+          <Logo 
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          
+          <Text style={styles.totalCars}>
+            Total 12 cars
+          </Text>
+        </View>
+      </View>
+
+      <Car />
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,16 +1,30 @@
 import { StyleSheet } from 'react-native';
+
 import theme from '../../global/themes/theme';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
 
     backgroundColor: theme.colors.background_primary,
   },
-  title: {
-    fontSize: 30,
-    fontFamily: theme.fonts.primary_400
+  header: {
+    width: '100%',
+    height: 113,
+
+    backgroundColor: theme.colors.header,
+    justifyContent: 'flex-end',
+    paddingVertical: 32,
+    paddingHorizontal: 24,
+  },
+  headerContent: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  totalCars: {
+    fontSize: 15,
+    fontFamily: theme.fonts.primary_400,
+    color: theme.colors.text,
   }
 });
